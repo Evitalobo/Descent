79b9c48f54a5bd79e1f5daa2bc961e865e4bbc39
JamesCoulter <38223465+JamesCoulter@users.noreply.github.com>
2018-11-11 17:46:11 -0800

    Add files via upload

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed = 15.0f;
+
+	// Use this for initialization
+	void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float moveForwardBack = Input.GetAxis("Vertical") * moveSpeed;
+        float moveToSide = Input.GetAxis("Horizontal") * moveSpeed;
+        moveForwardBack *= Time.deltaTime;
+        moveToSide *= Time.deltaTime;
+        transform.Translate(moveToSide, 0, moveForwardBack);
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+	}
+}
